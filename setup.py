8eb70d53f9e147eb47fa20370854efd6eb11fab2
Daniel Danis <daniel.danis@jax.org>
2019-09-02 15:37:59 -0400

    Update .gitignore, add url metadata

--- setup.py
+++ setup.py
@@ -8,14 +8,15 @@ setuptools.setup(name="ddplt",
                  packages=["ddplt"],
                  long_description=io.open('README.md', encoding='utf-8').read(),
                  long_description_content_type='text/markdown',
-                 license='GPLv3',
+
                  install_requires=['matplotlib>=3.1'
                                    'numpy>=1.16',
                                    'pandas>=0.23'],
 
                  author="Daniel Danis",
                  author_email="daniel.gordon.danis@gmail.com",
-                 url="",
+                 url="https://github.com/ielis/ddplt",
                  description="Useful utility functions for evaluation of ML",
+                 license='GPLv3',
                  keywords="plot machine learning evaluation metrics",
                  test_suite='tests')

c764792f15986886067413f222bb253594f93dab
Daniel Danis <daniel.danis@jax.org>
2020-10-10 20:50:07 +0200

    Update imports

--- ddplt/__init__.py
+++ ddplt/__init__.py
@@ -2,4 +2,4 @@ import pkg_resources
 
 __version__ = pkg_resources.get_distribution('ddplt').version
 
-from .heatmaps import plot_confusion_heatmap, plot_correlation_heatmap
+from . import heatmaps
